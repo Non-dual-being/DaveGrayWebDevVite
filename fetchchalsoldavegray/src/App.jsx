e5d89fc6e6b73680605871f6439b3f120f18fb64
Non-dual-being <kevindeschepper@hotmail.com>
2025-02-09 15:20:58 +0100

    first

---
+++ fetchchalsoldavegray/src/App.jsx
@@ -0,0 +1,42 @@
+import { use, useState } from 'react'
+import { useEffect } from 'react';
+import Form from './Form.jsx';
+
+
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('users');
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    const fectchItems = async() => {
+      try {
+        const reponse = await fetch(`${API_URL}${reqType}`);
+        const myitem = await reponse.json();
+        setItem(myitem);
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fectchItems()
+    /**
+     * no immidiate evoke nodig met await voor fetchitems omdat er geen return is
+     * Het gaat om de state bijhouden
+     */
+
+  }, [reqType])
+
+  return (
+    <div>
+    <Form 
+      reqType = {reqType}
+      setReqType = {setReqType}
+    />
+
+  </div>
+  )
+}
+
+export default App
---
+++ fetchchalsoldavegray/src/Form.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Button from './Button.jsx';
+
+
+
+const Form = ({ reqType, setReqType}) => {
+  return (
+    <form
+        onSubmit={(e) => e.preventDefault()}   
+        >
+        <Button 
+            buttonText = "users"
+            reqType = {reqType}
+            setReqType = {setReqType}
+        />
+        <Button 
+            buttonText = "posts"
+            reqType = {reqType}
+            setReqType = {setReqType}
+        />
+        <Button 
+            buttonText = "comments"
+            ReqType = {reqType}
+            setReqType = {setReqType}
+        />
+
+    </form>
+  )
+}
+
+export default Form
